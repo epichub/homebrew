3c01613633c044b4199417db5b50e4fddba38403
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2015-09-11 12:38:40 +0200

    bumped the davmail version

--- Library/Formula/davmail.rb
+++ Library/Formula/davmail.rb
@@ -1,8 +1,8 @@
 class Davmail < Formula
   desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
   homepage "http://davmail.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/davmail/davmail/4.5.1/davmail-4.5.1-2303.zip"
-  sha256 "d69f0c8cf6cf5b76c1fb0ff82fe7cecd22d470c83206fa3fecbe486c758c080d"
+  url "https://downloads.sourceforge.net/project/davmail/davmail/4.6.2/davmail-4.6.2-2377.zip"
+  sha256 "549c3023615ea3f69822ecd39b3f6eda2ef2ad4f5625bc54e4bc112a8ff88260"
 
   def install
     libexec.install Dir["*"]
